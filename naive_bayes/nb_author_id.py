a28a0ba53c6c021d2b514c8f4df7c09c2bb1b3e0
richardcynt <645309003@qq.com>
2017-11-27 16:30:35 +0800

    完成课程32迷你项目

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,9 +9,11 @@
     Sara has label 0
     Chris has label 1
 """
-    
+   
 import sys
 from time import time
+from sklearn.naive_bayes import GaussianNB
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -22,12 +24,19 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0, 3), "s"
 
+t1 = time()
+pred = clf.predict(features_test)
+print "predict time:", round(time() - t1, 3), "s"
 
+accuracy = clf.score(features_test, labels_test)
+print 'accuracy: ', accuracy
 #########################################################
 
 
