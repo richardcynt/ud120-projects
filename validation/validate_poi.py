996661653d42362fe936bb1772479354db8d1c6e
richardcynt <645309003@qq.com>
2017-12-15 20:17:30 +0800

    完成课程44交叉验证迷你程序

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -28,5 +28,17 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
+features_train, features_test, labels_train, labels_test = \
+train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf = DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+print 'accuracy: %.3f' % acc
 
